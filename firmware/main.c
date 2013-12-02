e9181a020223e05f5d7ea4144be3a1c9a7f43137
Kevin Loney <kevin.loney@gmail.com>
2013-12-02 02:14:25 -0700

    Updated code to use internal analog comparator instead of an external LM211 and the external interrupt

--- firmware/main.c
+++ firmware/main.c
@@ -2,7 +2,7 @@
 Name: main.c
 Author: Kevin Loney (kevin.loney@brainsinjars.com)
 Copyright: 2013
-Description: Basic control code for version 3.5 of the Police Public Call Box PCB
+Description: Basic control code for version 3.6 of the Police Public Call Box PCB
 
 License:
 This program is free software; you can redistribute it and/or
@@ -27,25 +27,29 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <avr/sfr_defs.h>
 #include <util/delay.h>
 
-volatile uint8_t active = 0;
+#define LERP(v, l0, h0, l1, h1)	(((h1) - (l1)) * ((v) - (l0)) / ((h0) - (l0)) + (l1))
 
-volatile unsigned long milliseconds = 0;
+#define TIME_CHARGE			2000
+#define TIME_DISCHARGE		2000
 
-const unsigned long time_debounce = 200;
-const unsigned long time_charge = 2000;
-const unsigned long time_discharge = 2000;
-const unsigned long time_on = 20000;
+#define STEPS_CHARGE		200
+#define STEPS_DISCHARGE		200
+
+#define DELAY_CHARGE		(TIME_CHARGE / STEPS_CHARGE)
+#define DELAY_DISCHARGE		(TIME_DISCHARGE / STEPS_DISCHARGE)
+
+#define TOTAL_LOOPS			5
 
 void setup(void);
 void loop(void);
 void snooze(void);
 
 /*
-PB0 - Roof Lantern (PWM)
-PB1 - Unused
-PB2 - Resistive Touch Input (AIN1)
+PB0 - Resistive Touch Input (AIN0)
+PB1 - Resistive Touch Level (AIN1)
+PB2 - Interior Lights
 PB3 - Unused
-PB4 - Interior Lights
+PB4 - Roof Lantern (PWM)
 PB5 - Unused (RESET)
 */
 
@@ -59,73 +63,54 @@ void setup(void) {
 	PORTB |= 0x3f; // Enable all pull-ups
 
 	// Outputs
-	PORTB &= ~(_BV(PORTB0) | _BV(PORTB4)); // Make all outputs low
-	DDRB |= _BV(DDB0) | _BV(DDB4); // Enable output pins
+	PORTB &= ~(_BV(PORTB2) | _BV(PORTB4)); // Make all outputs low
+	DDRB |= _BV(DDB2) | _BV(DDB4); // Enable output pins
 
-	// PWM Configuration
-	power_timer0_enable();
-	TCCR0A |= _BV(WGM01) | _BV(WGM00); // PB0 = fast non-inverting PWM
-	TCCR0B |= _BV(CS00); // Activate
-	OCR0A = 0xff;
-	power_timer0_disable();
+	// Analog Comparator
+	PORTB &= ~(_BV(PORTB0) | _BV(PORTB1)); // Disable pull-ups on AIN0 and AIN1
+	ACSR = 0x00; // Enable the analog comparator
+
+	ACSR |= _BV(ACIE); // Enable analog comparator interrupt
+	ACSR |= _BV(ACIS1); // Trigger interrupt on falling edge
+
+	ADCSRB &= ~(_BV(ACME)); // Use AIN1 (PB1) as the negative input
 
 	// Time configuration
 	power_timer1_enable();
-	TCCR1 = _BV(CTC1) | (7 << CS10);	// CTC  mode, div64 
-	OCR1C = 0.001 * F_CPU/64 - 1;		// 1ms, F_CPU @16MHz, div64 
-	TIMSK |= _BV(OCIE1A);
+	TCCR1 = 0<<PWM1A | 0<<COM1A0 | 1<<CS10;
+	GTCCR = 1<<PWM1B | 2<<COM1B0;
+	OCR1B = 0x00;
 	power_timer1_disable();
-
-	// External Interrupt
-	GIMSK |= _BV(INT0);
-	MCUCR &= ~(_BV(ISC01) | _BV(ISC00));
 
 	sei();
 }
 
-ISR(INT0_vect, ISR_NAKED) {
+ISR(ANA_COMP_vect, ISR_NAKED) {
 	reti();
 }
 
-ISR(TIMER1_COMPA_vect) { 
-	++milliseconds;
-}
-
 void loop(void) {
-	uint8_t state = 0, intensity = 0;
-	unsigned long start = milliseconds, finish = start + time_charge, delta;
+	unsigned long loop, level;
 
-	power_timer0_enable();
 	power_timer1_enable();
 
-	PORTB |= _BV(PORTB4); // Turn on interior lights
+	PORTB |= _BV(PORTB2); // Turn on interior lights
 
-	OCR0A = 0xFF;
-	TCCR0A |= _BV(COM0A1) | _BV(COM0A0); // Enable PWM output on PB0
-
-	while(milliseconds < time_on) {
-		unsigned long now = milliseconds;
-
-		if(now > finish) {
-			state = !state;
-			start = finish;
-			delta = (state ? time_discharge : time_charge);
-			finish = start + delta;
+	for(loop = 0; loop < TOTAL_LOOPS; ++loop) {
+		for(level = 0; level < STEPS_CHARGE; ++level) {
+			OCR1B = LERP(level, 0, STEPS_CHARGE, 0, 255) & 0xff;
+			_delay_ms(DELAY_CHARGE);
 		}
-
-		intensity = 0xff * (now - start) / delta;
-		if(!state) {
-			intensity = 0xff - intensity;
+		for(level = 0; level < STEPS_DISCHARGE; ++level) {
+			OCR1B = 0xff - LERP(level, 0, STEPS_DISCHARGE, 0, 255);
+			_delay_ms(DELAY_DISCHARGE);
 		}
-
-		OCR0A = intensity;
 	}
 
-	TCCR0A &= ~(_BV(COM0A1) | _BV(COM0A0)); // Disable PWM output on PB0
-	PORTB &= ~_BV(PORTB4); // Turn off interior lights
+	OCR1B = 0x00;
+	PORTB &= ~_BV(PORTB2); // Turn off interior lights
 
 	power_timer1_disable();
-	power_timer0_disable();
 }
 
 void snooze(void) {
